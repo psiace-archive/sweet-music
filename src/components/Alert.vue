066e6704abcb032c744b25b5683e3173f9e5987f
Chojan Shang <psiace@outlook.com>
2019-12-16 23:58:31 +0800

    :beers: Add alert component.

---
+++ src/components/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="alert">
+    <h3>
+      <slot></slot>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.alert {
+  background-color: #ff5252;
+  border-bottom: 2px solid #eee;
+  flex-shrink: 0;
+}
+a {
+  color: #eee;
+}
+h3 {
+  font-size: 1.5rem;
+  margin: 0.5em 1em;
+  text-align: center;
+}
+</style>
