fc60b6b540cf135be819cf1db3e54a109dc08332
Chojan Shang <psiace@outlook.com>
2019-12-21 17:02:55 +0800

    :lipstick: Better font color.

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #e7ebf0;
   height: 100vh;
   width: 100vw;
   margin: 0;
