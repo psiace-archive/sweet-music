a7167d492bd7d2d325daf6a8db235eb41a5641f0
Chojan Shang <psiace@outlook.com>
2019-12-26 02:51:53 +0800

    :sparkles: Add acf2plus.

---
+++ src/lib/PitchFinder/detectors/acf2plus.js
@@ -0,0 +1,68 @@
+const DEFAULT_SAMPLE_RATE = 44100;
+
+module.exports = function (config = {}) {
+    const sampleRate = config.sampleRate || DEFAULT_SAMPLE_RATE;
+
+    // Implements the ACF2+ algorithm
+    return function ACF2PLUSDetector(float32AudioBuffer) {
+        "use strict";
+
+        const maxShift = float32AudioBuffer.length;
+
+        let rms = 0;
+        let frames, thres, i, j, u, aux1, aux2, tmp;
+
+        for (i = 0; i < maxShift; i++) {
+            tmp = float32AudioBuffer[i];
+            rms += tmp * tmp;
+        }
+
+        rms = Math.sqrt(rms / maxShift);
+
+        if (rms < 0.01)
+            // not enough signal
+            return -1;
+
+        (aux1 = 0), (aux2 = maxShift - 1), (thres = 0.2);
+        for (i = 0; i < maxShift / 2; i++)
+            if (Math.abs(float32AudioBuffer[i]) < thres) {
+                aux1 = i;
+                break;
+            }
+        for (i = 1; i < maxShift / 2; i++)
+            if (Math.abs(float32AudioBuffer[maxShift - i]) < thres) {
+                aux2 = maxShift - i;
+                break;
+            }
+
+        frames = float32AudioBuffer.slice(aux1, aux2);
+        const framesLength = frames.length;
+
+        let calcSub = new Array(framesLength).fill(0);
+        for (i = 0; i < framesLength; i++)
+            for (j = 0; j < framesLength - i; j++)
+                calcSub[i] = calcSub[i] + frames[j] * frames[j + i];
+
+        u = 0;
+        while (calcSub[u] > calcSub[u + 1]) u++;
+        let maxval = -1,
+            maxpos = -1;
+        for (i = u; i < framesLength; i++) {
+            if (calcSub[i] > maxval) {
+                maxval = calcSub[i];
+                maxpos = i;
+            }
+        }
+
+        let T0 = maxpos;
+
+        const y1 = calcSub[T0 - 1],
+            y2 = calcSub[T0],
+            y3 = calcSub[T0 + 1];
+        let a = (y1 + y3 - 2 * y2) / 2;
+        let b = (y3 - y1) / 2;
+        if (a) T0 = T0 - b / (2 * a);
+
+        return sampleRate / T0;
+    };
+};
--- src/lib/PitchFinder/index.js
+++ src/lib/PitchFinder/index.js
@@ -1,8 +1,10 @@
 const AMDF = require("./detectors/amdf");
+const ACF2PLUS = require("./detectors/acf2plus")
 
 const frequencies = require("./tools/frequencies");
 
 module.exports = {
+  ACF2PLUS,
   AMDF,
   frequencies,
 };
