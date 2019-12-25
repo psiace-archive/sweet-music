ee31d2dd49876c14f7e29a869e82f868a74adaa9
Chojan Shang <psiace@outlook.com>
2019-12-26 03:43:22 +0800

    :fire: Clean code.

--- src/lib/PitchFinder/index.js
+++ src/lib/PitchFinder/index.js
@@ -1,10 +1,5 @@
-const AMDF = require("./detectors/amdf");
 const ACF2PLUS = require("./detectors/acf2plus")
-
-const frequencies = require("./tools/frequencies");
 
 module.exports = {
   ACF2PLUS,
-  AMDF,
-  frequencies,
 };
